3988cd444564f34fa9c3e95dc76c5b7e090e3c09
Subhranil Biswas <biswassubhranil2004@gmail.com>
2026-04-23 06:36:13 +0530

    Initial commit

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080/api/v1',
+    // baseURL: 'http://localhost:8080/api/v1',
+    baseURL:'https://internship-demo-backend.onrender.com/api/v1'
 });
 
 // Automatically attach JWT to requests if it exists in session
